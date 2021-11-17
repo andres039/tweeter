7c77349d7300f9888c1c3f58cd65740230f3ecdb
Andres <andres039039@hotmail.com>
2021-11-17 15:44:16 -0500

    tweets are dynamically rendered

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,20 +3,38 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const example = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
   },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
   },
-  created_at: 1461116232227,
-};
-const createTweetElement = function (tweet) {
-  return `<section class="tweet">
-  <article>
+];
+
+//jquery
+
+$(document).ready(function () {
+  const createTweetElement = function (tweet) {
+    //article has to be the outside type
+    let $tweet = ` 
+  <article class="tweet">
     <header>
       <div class='avatar'>
       <img src=${tweet.user.avatars}>
@@ -40,7 +58,16 @@ const createTweetElement = function (tweet) {
           </div>
           </footer>
         </article>
-      </section>`;
-};
+      `;
+    return $tweet;
+  };
 
-console.log(createTweetElement(example));
+  const renderTweets = function (arr) {
+    const $container = $(".container");
+    $.each(arr, (key) => {
+      $container.append(createTweetElement(arr[key]));
+    });
+    return $container;
+  };
+  renderTweets(data);
+});
