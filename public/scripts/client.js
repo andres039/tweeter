98d7bbd30040b7641c8e524f2529cd3263d3adf7
Andres <andres039039@hotmail.com>
2021-11-17 17:35:40 -0500

    using ajax renders tweets that are fetched from the /tweets locations

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,38 +3,40 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac",
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     created_at: 1461116232227,
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd",
+//     },
+//     content: {
+//       text: "Je pense , donc je suis",
+//     },
+//     created_at: 1461113959088,
+//   },
+// ];
 
-//jquery
+
 
 $(document).ready(function () {
+
   const createTweetElement = function (tweet) {
     //article has to be the outside type
     let $tweet = ` 
-  <article class="tweet">
+    <section class='tweet'>
+  <article>
     <header>
       <div class='avatar'>
       <img src=${tweet.user.avatars}>
@@ -58,6 +60,7 @@ $(document).ready(function () {
           </div>
           </footer>
         </article>
+        </section>
       `;
     return $tweet;
   };
@@ -69,5 +72,34 @@ $(document).ready(function () {
     });
     return $container;
   };
-  renderTweets(data);
+  
+
+  //Submit form
+
+  $( ".textarea" ).submit(function( event ) {
+   
+    
+    event.preventDefault();
+    console.log('serialzied:', $( this ).serialize() );
+
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/tweets",
+      data: $( this ).serialize()
+    })
+  });
+
+  //fetching tweets from /tweets page
+
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:8080/tweets',
+    })
+    .then(function (tweet) {
+      renderTweets(tweet);
+  });
+  }
+  loadTweets()
 });
+
